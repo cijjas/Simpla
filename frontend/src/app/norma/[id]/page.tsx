d940acccb241805fb2949e9924e817a8ea7deea5
cijjas <cijjas@itba.edu.ar>
2025-05-05 18:54:29 -0300

    fix: change [id] paramas to Promise

--- frontend/src/app/norma/[id]/page.tsx
+++ frontend/src/app/norma/[id]/page.tsx
@@ -3,12 +3,12 @@ import { NormaHeader } from '@/features/norma/NormaHeader';
 import { NormaBody } from '@/features/norma/NormaBody';
 import { notFound } from 'next/navigation';
 
-export default async function NormaPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const norma = await getNormaDetalle(Number(params.id));
+interface Props {
+  params: Promise<{ id: string }>;
+}
+export default async function NormaPage({ params }: Props) {
+  const { id } = await params;
+  const norma = await getNormaDetalle(Number(id));
   if (!norma) notFound();
 
   return (
